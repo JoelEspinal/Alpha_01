6b367c5f0046b51eefe5fc24839255559453f209
Adrian Escobar <adrian080891@gmail.com>
2013-10-27 16:10:06 -0400

    index puesto

--- application/views/home/index.blade.php
+++ application/views/home/index.blade.php
@@ -1,57 +1,123 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
-<head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-	<title>Laravel: A Framework For Web Artisans</title>
-	<meta name="viewport" content="width=device-width">
-	{{ HTML::style('laravel/css/style.css') }}
-</head>
-<body>
-	<div class="wrapper">
-		<header>
-			<h1>Laravel</h1>
-			<h2>A Framework For Web Artisans</h2>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
 
-			<p class="intro-text" style="margin-top: 45px;">
-			</p>
-		</header>
-		<div role="main" class="main">
-			<div class="home">
-				<h2>Learn the terrain.</h2>
+    <title>Alpha 01</title>
 
-				<p>
-					You've landed yourself on our default home page. The route that
-					is generating this page lives at:
-				</p>
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.css" rel="stylesheet">
 
-				<pre>{{ path('app') }}routes.php</pre>
+    <!-- Add custom CSS here -->
+    <link href="css/blog-home.css" rel="stylesheet">
 
-				<p>And the view sitting before you can be found at:</p>
+  </head>
 
-				<pre>{{ path('app') }}views/home/index.blade.php</pre>
+  <body>
 
-				<h2>Grow in knowledge.</h2>
+    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="index.html">Start Bootstrap</a>
+        </div>
 
-				<p>
-					Learning to use Laravel is amazingly simple thanks to
-					its {{ HTML::link('docs', 'wonderful documentation') }}.
-				</p>
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse navbar-ex1-collapse">
+          <ul class="nav navbar-nav">
+            <li><a href="#about">About</a></li>
+            <li><a href="#services">Services</a></li>
+            <li><a href="#contact">Contact</a></li>
+          </ul>
+        </div><!-- /.navbar-collapse -->
+      </div><!-- /.container -->
+    </nav>
 
-				<h2>Create something beautiful.</h2>
+    <div class="container">
 
-				<p>
-					Now that you're up and running, it's time to start creating!
-					Here are some links to help you get started:
-				</p>
+      <div class="row">
+        <div class="col-lg-8">
+        
+          <!-- blog entry -->
+          <h1><a href="#">A Blog Home Template for Bootstrap 3</a></h1>
+          <p class="lead">by <a href="index.html">Start Bootstrap</a></p>
+          <hr>
+          <p><span class="glyphicon glyphicon-time"></span> Posted on August 28, 2013 at 10:00 PM</p>
+          <hr>
 
-				<ul class="out-links">
-					<li><a href="http://laravel.com">Official Website</a></li>
-					<li><a href="http://forums.laravel.com">Laravel Forums</a></li>
-					<li><a href="http://github.com/laravel/laravel">GitHub Repository</a></li>
-				</ul>
-			</div>
-		</div>
-	</div>
-</body>
+            <div>
+              <iframe width="720" height="400" src="//www.youtube.com/embed/eOG90Q8EfRo" frameborder="0" allowfullscreen></iframe>
+            </div>
+          <hr>
+          <p>This is a very basic starter template for a blog homepage. It makes use of Glyphicons that are built into Bootstrap 3, and it makes use of the Pager at the bottom of the page. Make sure you get the Glyphicons files by downloading the entire <code>/fonts</code> directory that you can download in the source files or at <a href="http://getbootstrap.com">getbootstrap.com</a>. That directory has all of the web fonts in it which makes Glyphicons work.</p>
+          <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
+                    
+          <hr>
+          
+        </div>
+        
+        <div class="col-lg-4">
+          <div class="well">
+            <h4>Alpha 01 Search</h4>
+            <div class="input-group">
+              <input type="text" class="form-control">
+              <span class="input-group-btn">
+                <button class="btn btn-default" type="button"><span class="glyphicon glyphicon-search"></span></button>
+              </span>
+            </div><!-- /input-group -->
+          </div><!-- /well -->
+          <div class="well">
+            <h4>Popular Categories</h4>
+              <div class="row">
+                <div class="col-lg-6">
+                  <ul class="list-unstyled">
+                    <li><a href="#dinosaurs">Dinosaurs</a></li>
+                    <li><a href="#spaceships">Spaceship</a>s</li>
+                    <li><a href="#fried-foods">Fried Foods</a></li>
+                    <li><a href="#wild-animals">Wild Animals</a></li>
+                  </ul>
+                </div>
+                <div class="col-lg-6">
+                  <ul class="list-unstyled">
+                    <li><a href="#alien-abductions">Alien Abductions</a></li>
+                    <li><a href="#business-casual">Business Casual</a></li>
+                    <li><a href="#robots">Robots</a></li>
+                    <li><a href="#fireworks">Fireworks</a></li>
+                  </ul>
+                </div>
+              </div>
+          </div><!-- /well -->
+          <div class="well">
+            <h4>Side Widget Well</h4>
+            <p>Bootstrap's default well's work great for side widgets! What is a widget anyways...?</p>
+          </div><!-- /well -->
+        </div>
+      </div>
+      
+      <hr>
+      
+      <footer>
+        <div class="row">
+          <div class="col-lg-12">
+            <p>Copyright &copy; Company 2013 &middot; Facebook &middot; Twitter &middot; Google+</p>
+          </div>
+        </div>
+      </footer>
+
+    </div><!-- /.container -->
+
+    <!-- Bootstrap core JavaScript -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <!-- Make sure to add jQuery - download the most recent version at http://jquery.com/ -->
+    <script src="js/jquery.js"></script>
+    <script src="js/bootstrap.js"></script>
+  </body>
 </html>
